3415c42583c308792bc9c5d980d07d2aae77c40c
NP-e <75404710+NP-e@users.noreply.github.com>
2020-12-02 19:44:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function flipimage() {
+    document.getElementById("image1").src = "https://images.unsplash.com/photo-1530618655905-2063770256bd?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ";
+}
+
+function flipimage2() {
+    document.getElementById("image2").src = "https://images.unsplash.com/photo-1511431833882-509a76113392?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ";
+}
